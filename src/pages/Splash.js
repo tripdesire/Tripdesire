5cc8c4ea8490a279f57ccf1e15c1fc53b86fe9cc
kamlesh gangwar <kamlesh@webiixx.com>
2019-11-15 12:37:46 +0530

    Splash update

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,81 +1,18 @@
 import React, { PureComponent } from "react";
 import { Text } from "../components";
 import { Image, ImageBackground, Dimensions } from "react-native";
-import { connect } from "react-redux";
-import { DomSugg, IntSugg, DomHotelSugg, BusSugg } from "../store/action";
-import Service from "../service";
-import axios from "axios";
+
 const { height, width } = Dimensions.get("window");
+
 class Splash extends React.PureComponent {
   constructor(props) {
     super(props);
-    // setTimeout(() => {
-    //   this.props.navigation.navigate("Bus");
-    // }, 2000);
   }
-
-  // getDomesticSeggestion() {
-  //   return Service.get("/Flights/Airports?flightType=1");
-  // }
-
-  // getInternationalSeggestion() {
-  //   return Service.get("/Flights/Airports?flightType=2");
-  // }
-  getHotelSugesstion() {
-    return Service.get("/Hotels/Cities?hoteltype=1");
-  }
-  // getBusSuggestion() {
-  //   return Service.get("/Buses/Sources");
-  // }
 
   componentDidMount() {
-    const {
-      domesticSuggestion,
-      internationalSuggestion,
-      domesticHotelSuggestion,
-      busSuggestion
-    } = this.props;
-    if (
-      domesticSuggestion.length == 0 ||
-      internationalSuggestion.length == 0 ||
-      domesticHotelSuggestion.length == 0 //||
-      // busSuggestion.length == 0
-    ) {
-      axios
-        .all([
-          //  this.getDomesticSeggestion(),
-          //  this.getInternationalSeggestion(),
-          this.getHotelSugesstion()
-          //  this.getBusSuggestion()
-        ])
-        .then(
-          axios.spread(hotel => {
-            //   this.props.DomSugg(domestic.data);
-            //  this.props.IntSugg(international.data);
-            this.props.DomHotelSugg(hotel.data);
-            //    this.props.BusSugg(bus.data);
-            this.props.navigation.navigate("DrawerNavigator"); //DrawerNavigator
-          })
-        );
-    } else {
+    setTimeout(() => {
       this.props.navigation.navigate("DrawerNavigator");
-      axios
-        .all([
-          // this.getDomesticSeggestion(),
-          // this.getInternationalSeggestion(),
-          this.getHotelSugesstion()
-          // this.getBusSuggestion()
-        ])
-        .then(
-          axios.spread(hotel => {
-            // this.props.DomSugg(domestic.data);
-            //  this.props.IntSugg(international.data);
-            this.props.DomHotelSugg(hotel.data);
-            // this.props.BusSugg(bus.data);
-            // Both requests are now complete
-          })
-        );
-    }
+    }, 2000);
   }
 
   render() {
@@ -104,17 +41,4 @@ class Splash extends React.PureComponent {
   }
 }
 
-const mapDispatchToProps = {
-  DomSugg,
-  IntSugg,
-  DomHotelSugg,
-  BusSugg
-};
-const mapStateToProps = state => ({
-  domesticSuggestion: state.domesticSuggestion,
-  internationalSuggestion: state.internationalSuggestion,
-  domesticHotelSuggestion: state.domesticHotelSuggestionReducer,
-  busSuggestion: state.busSuggestion
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Splash);
+export default Splash;
