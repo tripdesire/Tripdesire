2c7769a6cc0015f3aca22437c9b82fe8b159c46b
Abhishek Singh <abhishek@webiixx.com>
2019-12-03 15:48:21 +0530

    Fix

--- src/pages/Cab/Filter.js
+++ src/pages/Cab/Filter.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import { Button, Icon, Text, CheckBox } from "../../components";
 import { uniq, max, min } from "lodash";
-import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import RangeSlider from "rn-range-slider";
 
 function Filter({ data, onBackPress, filterValues, onChangeFilter, filter }) {
