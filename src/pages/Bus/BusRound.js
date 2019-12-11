b001350510bbb66bf93144e8f1fc01b7f52969ef
kamlesh gangwar <kamlesh@webiixx.com>
2019-12-11 12:33:43 +0530

    fix bus

--- src/pages/Bus/BusRound.js
+++ src/pages/Bus/BusRound.js
@@ -9,6 +9,7 @@ import moment from "moment";
 import RenderRound from "./RenderRound";
 import Filter from "./Filter";
 
+const { width, height } = Dimensions.get("window");
 class BusRound extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -18,7 +19,11 @@ class BusRound extends React.PureComponent {
       onwardBus: [],
       returnBus: [],
       selectedOnward: 0,
-      selectedReturn: 0
+      selectedReturn: 0,
+      index: 0,
+      swiperIndex: 0,
+      onwardFare: "",
+      returnFare: ""
     };
   }
 
@@ -35,6 +40,7 @@ class BusRound extends React.PureComponent {
         }
         this.setState({
           onwardBus: data.AvailableTrips,
+          onwardFare: data.AvailableTrips.length > 0 ? data.AvailableTrips[0].Fares : 0,
           filteredBuses: data.AvailableTrips,
           loader: false
         });
@@ -43,140 +49,113 @@ class BusRound extends React.PureComponent {
         this.setState({ loader: false });
       });
 
-    let newData = Object.assign({}, params);
-    newData.sourceName = params.destinationName;
-    newData.destinationName = params.sourceName;
-    newData.sourceId = params.destinationId;
-    newData.destinationId = params.sourceId;
-    newData.journeyDate = params.returnDate;
-    newData.returnDate = "";
-    console.log(newData);
+    // let newData = Object.assign({}, params);
+    // newData.sourceName = params.destinationName;
+    // newData.destinationName = params.sourceName;
+    // newData.sourceId = params.destinationId;
+    // newData.destinationId = params.sourceId;
+    // newData.journeyDate = params.returnDate;
+    // newData.returnDate = "";
+    // console.log(newData);
 
-    this.setState({ loader: true });
-    etravosApi
-      .get("/Buses/AvailableBuses", newData)
-      .then(({ data }) => {
-        console.log(data.AvailableTrips);
-        if (data.AvailableTrips.length == 0) {
-          this.setState({ nofound: data.AvailableTrips.length, loader: false });
-          Toast.show("Data not found.", Toast.LONG);
-        }
-        this.setState({
-          returnBus: data.AvailableTrips,
-          //   filteredBuses: data.AvailableTrips,
-          loader: false
-        });
-      })
-      .catch(() => {
-        this.setState({ loader: false });
-      });
+    // this.setState({ loader: true });
+    // etravosApi
+    //   .get("/Buses/AvailableBuses", newData)
+    //   .then(({ data }) => {
+    //     console.log(data.AvailableTrips);
+    //     if (data.AvailableTrips.length == 0) {
+    //       this.setState({ nofound: data.AvailableTrips.length, loader: false });
+    //       Toast.show("Data not found.", Toast.LONG);
+    //     }
+    //     this.setState({
+    //       returnBus: data.AvailableTrips,
+    //       returnFare: data.AvailableTrips.length > 0 ? data.AvailableTrips[0].Fares : 0,
+    //       //   filteredBuses: data.AvailableTrips,
+    //       loader: false
+    //     });
+    //   })
+    //   .catch(() => {
+    //     this.setState({ loader: false });
+    //   });
   }
 
   _renderItemOnward = ({ item, index }) => {
-    return <RenderRound item={item} index={index} />;
+    const { tripType, sourceName, destinationName } = this.props.navigation.state.params;
+    return (
+      <RenderRound
+        item={item}
+        index={index}
+        getBus={this._getBusOnward}
+        tripType={tripType}
+        sourceName={sourceName}
+        destinationName={destinationName}
+      />
+    );
   };
 
-  _renderItemReturn = ({ item, index }) => {
-    return <RenderRound item={item} index={index} />;
+  _onChangeIndex = ({ index }) => {
+    this.setState({ swiperIndex: index });
   };
 
-  _onPress = value => {};
+  _getBusOnward = (value, index) => {
+    console.log("hey");
+    this.setState({
+      onwardFare: value.Fares,
+      selectedOnward: index
+    });
+  };
 
   _keyExtractorOnward = (item, index) => "OnwardBus_" + index;
 
-  _keyExtractorReturn = (item, index) => "ReturnBus_" + index;
-
   render() {
-    const { onwardBus, returnBus, loader } = this.state;
+    const { onwardBus, returnBus, loader, index, swiperIndex, onwardFare, returnFare } = this.state;
+    const {
+      tripType,
+      sourceName,
+      destinationName,
+      journeyDate,
+      returnDate
+    } = this.props.navigation.state.params;
     return (
       <>
         <SafeAreaView style={{ flex: 0, backgroundColor: "#E5EBF7" }} />
         <SafeAreaView style={{ flex: 1, backgroundColor: "#ffffff" }}>
           <View style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-            <HeaderFlights
-              from="{from}"
-              to="{to}"
-              journey_date="{journey_date}"
-              return_date="{return_date}"
-              Adult="{Adult}"
-              Child="{Child}"
-              Infant="{Infant}"
-              className="{className}"
-              style={{ backgroundColor: "#E5EBF7", paddingBottom: 8 }}>
-              <Button
+            <View style={{ backgroundColor: "#E5EBF7" }}>
+              <View
                 style={{
                   flexDirection: "row",
-                  marginStart: "auto",
-                  paddingEnd: 8,
-                  paddingVertical: 16
+                  width: "100%"
                 }}>
-                <Icon name="filter" size={20} color="#5D89F4" type="MaterialCommunityIcons" />
-                <Text style={{ fontSize: 14, marginHorizontal: 5, color: "#717984" }}>
-                  Sort & Filter
-                </Text>
-              </Button>
-            </HeaderFlights>
-
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                margin: 16,
-                alignItems: "center"
-              }}>
-              <View>
-                <Text style={{ color: "#5F6D78" }}>Departure</Text>
-                <Text style={{ color: "#212C4C", fontSize: 18, fontWeight: "700" }}>
-                  $ onwardFare
-                </Text>
+                <Button onPress={() => this.props.navigation.goBack(null)} style={{ padding: 16 }}>
+                  <Icon name="md-arrow-back" size={24} />
+                </Button>
+                <View style={{ flex: 1, paddingTop: 16, paddingBottom: 8 }}>
+                  <View>
+                    <Text style={{ fontWeight: "700", fontSize: 16, marginHorizontal: 5 }}>
+                      {sourceName} to {destinationName}
+                    </Text>
+                    <Text style={{ fontSize: 12, marginHorizontal: 5, color: "#717984" }}>
+                      {journeyDate} {" - " + returnDate}
+                    </Text>
+                  </View>
+                </View>
+                <Button
+                  style={{
+                    flexDirection: "row",
+                    marginStart: "auto",
+                    paddingEnd: 8,
+                    paddingVertical: 16
+                  }}>
+                  <Icon name="filter" size={20} color="#5D89F4" type="MaterialCommunityIcons" />
+                  <Text style={{ fontSize: 14, marginHorizontal: 5, color: "#717984" }}>
+                    Sort & Filter
+                  </Text>
+                </Button>
               </View>
-              <View>
-                <Text style={{ color: "#5F6D78" }}>Return</Text>
-                <Text style={{ color: "#212C4C", fontSize: 18, fontWeight: "700" }}>
-                  $ returnFare
-                </Text>
-              </View>
-              <View>
-                <Text style={{ color: "#5F6D78" }}>Total</Text>
-                <Text style={{ color: "#212C4C", fontSize: 18, fontWeight: "700" }}>
-                  $ onwardFare + returnFare
-                </Text>
-              </View>
-              <Button style={{ backgroundColor: "#F68E1F", borderRadius: 15 }}>
-                <Text style={{ color: "#fff", paddingHorizontal: 10, paddingVertical: 4 }}>
-                  Book Now
-                </Text>
-              </Button>
-            </View>
-            <View
-              style={{
-                backgroundColor: "#DEDEDE",
-                height: 1,
-                marginHorizontal: 16,
-                marginVertical: 10
-              }}
-            />
-            <View
-              style={{
-                backgroundColor: "#FFFFFF",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-                marginHorizontal: 16
-              }}>
-              <Button
-                style={[styles.tabBtn, { backgroundColor: "#5B89F9" }]}
-                onPress={this._onPress("Depart")}>
-                <Text style={{ fontSize: 12, color: "#ffffff" }}>Depart</Text>
-              </Button>
-              <Button
-                style={[styles.tabBtn, { backgroundColor: "#ffffff" }]}
-                onPress={this._onPress("Return")}>
-                <Text style={{ fontSize: 12, color: "#000000" }}>Return</Text>
-              </Button>
             </View>
 
-            <SwiperFlatList index={index}>
+            <View style={{ flex: 4 }}>
               <FlatList
                 data={onwardBus}
                 keyExtractor={this._keyExtractorOnward}
@@ -184,15 +163,8 @@ class BusRound extends React.PureComponent {
                 contentContainerStyle={{ width, paddingHorizontal: 8 }}
                 extraData={this.state.selectedOnward}
               />
-              <FlatList
-                data={returnBus}
-                keyExtractor={this._keyExtractorReturn}
-                renderItem={this._renderItemReturn}
-                contentContainerStyle={{ width, paddingHorizontal: 8 }}
-                extraData={this.state.selectedReturn}
-              />
-            </SwiperFlatList>
-            {loader && <ActivityIndicator />}
+              {loader && <ActivityIndicator />}
+            </View>
           </View>
         </SafeAreaView>
       </>
