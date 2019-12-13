be7a47c11bdf839d625700363ca041bfde42cc30
Abhishek Singh <abhishek@webiixx.com>
2019-12-13 18:07:19 +0530

    Issues

--- src/pages/Flight/FlightSearch.js
+++ src/pages/Flight/FlightSearch.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {View, StyleSheet, SafeAreaView} from "react-native";
-import {Button, Text} from "../../components";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import { Button, Text } from "../../components";
 import DomesticFlights from "./DomesticFlights";
 import InternationalFlights from "./InternationalFlights";
-import {Header} from "../../components";
+import { Header } from "../../components";
 
 class FlightSearch extends React.PureComponent {
   constructor(props) {
@@ -14,26 +14,26 @@ class FlightSearch extends React.PureComponent {
   }
 
   setDomesticActive = () => {
-    this.setState({flightType: 1});
+    this.setState({ flightType: 1 });
   };
   setInternationalActive = () => {
-    this.setState({flightType: 2});
+    this.setState({ flightType: 2 });
   };
 
   render() {
-    const {flightType} = this.state;
+    const { flightType } = this.state;
     return (
       <>
-        <SafeAreaView style={{flex: 0, backgroundColor: "#E4EAF6"}} />
-        <SafeAreaView style={{flex: 1, backgroundColor: "gray"}}>
-          <View style={{flexDirection: "column", flex: 1}}>
-            <View style={{backgroundColor: "#E4EAF6", flex: 1}}>
+        <SafeAreaView style={{ flex: 0, backgroundColor: "#E4EAF6" }} />
+        <SafeAreaView style={{ flex: 1, backgroundColor: "gray" }}>
+          <View style={{ flexDirection: "column", flex: 1 }}>
+            <View style={{ backgroundColor: "#E4EAF6", flex: 1 }}>
               <Header firstName="Flights" lastName="Search" />
             </View>
 
-            <View style={{height: 30, width: "100%"}}>
-              <View style={{flex: 2, backgroundColor: "#E4EAF6"}}></View>
-              <View style={{flex: 1, backgroundColor: "#FFFFFF"}}></View>
+            <View style={{ height: 30, width: "100%" }}>
+              <View style={{ flex: 2, backgroundColor: "#E4EAF6" }}></View>
+              <View style={{ flex: 1, backgroundColor: "#FFFFFF" }}></View>
               <View
                 style={{
                   flexDirection: "row",
@@ -43,42 +43,26 @@ class FlightSearch extends React.PureComponent {
                 <Button
                   style={{
                     backgroundColor: flightType == 1 ? "#5B89F9" : "#FFFFFF",
-                    elevation: 1,
-                    borderWidth: 1,
-                    borderColor: "#DDDDDD",
-                    height: 30,
-                    justifyContent: "center",
-                    paddingHorizontal: 50,
-                    borderBottomStartRadius: 5,
-                    borderTopStartRadius: 5
+                    ...styles.tabButtons
                   }}
                   onPress={this.setDomesticActive}>
-                  <Text style={{color: flightType == 1 ? "#FFFFFF" : "#000000", fontSize: 12}}>
+                  <Text style={{ color: flightType == 1 ? "#FFFFFF" : "#000000", fontSize: 12 }}>
                     Domestic
                   </Text>
                 </Button>
                 <Button
                   style={{
                     backgroundColor: flightType == 2 ? "#5B89F9" : "#FFFFFF",
-                    elevation: 1,
-                    height: 30,
-                    borderEndWidth: 1,
-                    borderTopWidth: 1,
-                    borderBottomWidth: 1,
-                    borderColor: "#DDDDDD",
-                    justifyContent: "center",
-                    paddingHorizontal: 50,
-                    borderBottomEndRadius: 5,
-                    borderTopEndRadius: 5
+                    ...styles.tabButtons
                   }}
                   onPress={this.setInternationalActive}>
-                  <Text style={{fontSize: 12, color: flightType == 2 ? "#FFFFFF" : "#000000"}}>
+                  <Text style={{ fontSize: 12, color: flightType == 2 ? "#FFFFFF" : "#000000" }}>
                     International
                   </Text>
                 </Button>
               </View>
             </View>
-            <View style={{backgroundColor: "#FFFFFF", flex: 4}}>
+            <View style={{ backgroundColor: "#FFFFFF", flex: 4 }}>
               {flightType == 1 ? <DomesticFlights /> : <InternationalFlights />}
             </View>
           </View>
@@ -87,5 +71,18 @@ class FlightSearch extends React.PureComponent {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  tabButtons: {
+    elevation: 1,
+    borderWidth: 1,
+    borderColor: "#DDDDDD",
+    height: 30,
+    justifyContent: "center",
+    paddingHorizontal: 50,
+    borderBottomStartRadius: 5,
+    borderTopStartRadius: 5
+  }
+});
 
 export default FlightSearch;
