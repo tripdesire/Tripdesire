9f9c0d71fb0340ee59f6b037792e106c8d43d66f
Abhishek Singh <abhishek@webiixx.com>
2020-01-13 11:10:36 +0530

    Change API

--- src/service/domainApi.js
+++ src/service/domainApi.js
@@ -6,8 +6,8 @@ import Toast from "react-native-simple-toast";
 class Service {
   constructor() {
     let service = axios.create({
-      //baseURL: "https://demo66.tutiixx.com/wp-json/wc/v2"
-      baseURL: "https://tripdesire.co/wp-json/wc/v2"
+      baseURL: "https://demo66.tutiixx.com/wp-json/wc/v2"
+      //  baseURL: "https://tripdesire.co/wp-json/wc/v2"
     });
     axiosRetry(service, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
     service.interceptors.response.use(this.handleSuccess, this.handleError);
